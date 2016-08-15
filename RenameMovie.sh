86f14c0b52ee79f44996708485c61ef34d689f30
Han Sooloo <hansooloo@gmail.com>
2016-08-15 10:11:06 -0400

    * Added extract directory variable. * Started using extract directory as the source of rename process.

--- RenameMovie.sh
+++ RenameMovie.sh
@@ -4,11 +4,12 @@ LOGFILE=`date +%Y-%m-%d_%H-%M-%S`
 FILEBOT_DIR="/root/FileBot"
 JD_DIR="/mnt/JDownloader"
 DOWNLOAD_DIR="${JD_DIR}/_01.Download_"
-OUTPUT_DIR="${JD_DIR}/_02.Format_"
+EXTRACT_DIR="${JD_DIR}/_02.Extract_"
+OUTPUT_DIR="${JD_DIR}/_03.Format_"
 RENAME_FORMAT="/root/FormatMovie.txt"
 
 ${FILEBOT_DIR}/filebot.sh                     \
-    ${DOWNLOAD_DIR}                           \
+    ${EXTRACT_DIR}                            \
     -script fn:amc                            \
         -non-strict                           \
         -no-xattr                             \
